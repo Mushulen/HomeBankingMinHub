d0c124307b0545ab4c5e9d944f276d7d2ed031f3
Emiliano Lencina <Yaleoxx@DESKTOP-ERRQB2K>
2024-02-13 03:25:07 -0300

    *Agregadas bases de los nuevos repositorios.

---
+++ Repositories/Interface/ICardRepository.cs
@@ -0,0 +1,12 @@
+﻿using HomeBankingMinHub.Models;
+using System.Collections.Generic;
+
+namespace HomeBankingMinHub.Repositories.Interface
+{
+    public interface ICardRepository
+    {
+        void Save(Card card);
+        Card FindById(long id);
+        IEnumerable<Card> GetAllCards();
+    }
+}
---
+++ Repositories/TransactionsRepository.cs
@@ -0,0 +1,31 @@
+﻿using HomeBankingMinHub.Models;
+using HomeBankingMinHub.Repositories;
+using HomeBankingMinHub.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeBankingMinHub.Repositories
+{
+    public class TransactionsRepository : RepositoryBase<Transactions>, ITransactionsRepository
+    {
+        public TransactionsRepository(HomeBankingContext repositoryContext) : base(repositoryContext)
+        {
+        }
+        public Transactions FindById(long id)
+        {
+            return FindByCondition(transaction => transaction.Id == id)
+                   .FirstOrDefault();
+        }
+        public IEnumerable<Transactions> GetAllTransactions()
+        {
+            return FindAll()
+                   .ToList();
+        }
+        public void Save(Transactions transaction)
+        {
+            Create(transaction);
+            SaveChanges();
+        }
+    }
+}
